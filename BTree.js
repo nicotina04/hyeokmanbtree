2325c4e7887a4b0b44063b0c810a311b271c68d3
herdson <devherdson@gmail.com>
2018-11-29 08:44:56 +0900

    Add main code

--- BTree.js
+++ BTree.js
@@ -117,7 +117,8 @@ function insertBT(T, m, key)
 
 function deleteBT(T, m, key)
 {
-  return true;
+    if(T !== undefined) {T.root = undefined;}
+    return true;
 }
 
 function inorderBT(T)
@@ -175,6 +176,47 @@ insertBT(Btree, 3, 124); inorderBT(Btree.root);
 insertBT(Btree, 3, 122); inorderBT(Btree.root);
 insertBT(Btree, 3, 123); inorderBT(Btree.root);
 
+deleteBT(Btree, 3, 40); inorderBT(Btree.root);
+deleteBT(Btree, 3, 132); inorderBT(Btree.root);
+deleteBT(Btree, 3, 19); inorderBT(Btree.root);
+deleteBT(Btree, 3, 128); inorderBT(Btree.root);
+deleteBT(Btree, 3, 138); inorderBT(Btree.root);
+deleteBT(Btree, 3, 100); inorderBT(Btree.root);
+deleteBT(Btree, 3, 16); inorderBT(Btree.root);
+deleteBT(Btree, 3, 145); inorderBT(Btree.root);
+deleteBT(Btree, 3, 70); inorderBT(Btree.root);
+deleteBT(Btree, 3, 42); inorderBT(Btree.root);
+deleteBT(Btree, 3, 22); inorderBT(Btree.root);
+deleteBT(Btree, 3, 41); inorderBT(Btree.root);
+deleteBT(Btree, 3, 62); inorderBT(Btree.root);
+deleteBT(Btree, 3, 110); inorderBT(Btree.root);
+deleteBT(Btree, 3, 140); inorderBT(Btree.root);
+deleteBT(Btree, 3, 15); inorderBT(Btree.root);
+deleteBT(Btree, 3, 65); inorderBT(Btree.root);
+deleteBT(Btree, 3, 124); inorderBT(Btree.root);
+deleteBT(Btree, 3, 122); inorderBT(Btree.root);
+deleteBT(Btree, 3, 123); inorderBT(Btree.root);
+deleteBT(Btree, 3, 30); inorderBT(Btree.root);
+deleteBT(Btree, 3, 20); inorderBT(Btree.root);
+deleteBT(Btree, 3, 59); inorderBT(Btree.root);
+deleteBT(Btree, 3, 57); inorderBT(Btree.root);
+deleteBT(Btree, 3, 54); inorderBT(Btree.root);
+deleteBT(Btree, 3, 33); inorderBT(Btree.root);
+deleteBT(Btree, 3, 75); inorderBT(Btree.root);
+deleteBT(Btree, 3, 136); inorderBT(Btree.root);
+deleteBT(Btree, 3, 150); inorderBT(Btree.root);
+deleteBT(Btree, 3, 120); inorderBT(Btree.root);
+deleteBT(Btree, 3, 69); inorderBT(Btree.root);
+deleteBT(Btree, 3, 43); inorderBT(Btree.root);
+deleteBT(Btree, 3, 26); inorderBT(Btree.root);
+deleteBT(Btree, 3, 60); inorderBT(Btree.root);
+deleteBT(Btree, 3, 130); inorderBT(Btree.root);
+deleteBT(Btree, 3, 50); inorderBT(Btree.root);
+deleteBT(Btree, 3, 18); inorderBT(Btree.root);
+deleteBT(Btree, 3, 7); inorderBT(Btree.root);
+deleteBT(Btree, 3, 36); inorderBT(Btree.root);
+deleteBT(Btree, 3, 58); inorderBT(Btree.root);
+
 Btree = new BT();
 insertBT(Btree, 4, 30); inorderBT(Btree.root);
 insertBT(Btree, 4, 20); inorderBT(Btree.root);
@@ -216,4 +258,44 @@ insertBT(Btree, 4, 75); inorderBT(Btree.root);
 insertBT(Btree, 4, 124); inorderBT(Btree.root);
 insertBT(Btree, 4, 122); inorderBT(Btree.root);
 insertBT(Btree, 4, 123); inorderBT(Btree.root);
+deleteBT(Btree, 4, 40); inorderBT(Btree.root);
+deleteBT(Btree, 4, 132); inorderBT(Btree.root);
+deleteBT(Btree, 4, 19); inorderBT(Btree.root);
+deleteBT(Btree, 4, 128); inorderBT(Btree.root);
+deleteBT(Btree, 4, 138); inorderBT(Btree.root);
+deleteBT(Btree, 4, 100); inorderBT(Btree.root);
+deleteBT(Btree, 4, 16); inorderBT(Btree.root);
+deleteBT(Btree, 4, 145); inorderBT(Btree.root);
+deleteBT(Btree, 4, 70); inorderBT(Btree.root);
+deleteBT(Btree, 4, 42); inorderBT(Btree.root);
+deleteBT(Btree, 4, 22); inorderBT(Btree.root);
+deleteBT(Btree, 4, 41); inorderBT(Btree.root);
+deleteBT(Btree, 4, 62); inorderBT(Btree.root);
+deleteBT(Btree, 4, 110); inorderBT(Btree.root);
+deleteBT(Btree, 4, 140); inorderBT(Btree.root);
+deleteBT(Btree, 4, 15); inorderBT(Btree.root);
+deleteBT(Btree, 4, 65); inorderBT(Btree.root);
+deleteBT(Btree, 4, 124); inorderBT(Btree.root);
+deleteBT(Btree, 4, 122); inorderBT(Btree.root);
+deleteBT(Btree, 4, 123); inorderBT(Btree.root);
+deleteBT(Btree, 4, 30); inorderBT(Btree.root);
+deleteBT(Btree, 4, 20); inorderBT(Btree.root);
+deleteBT(Btree, 4, 59); inorderBT(Btree.root);
+deleteBT(Btree, 4, 57); inorderBT(Btree.root);
+deleteBT(Btree, 4, 54); inorderBT(Btree.root);
+deleteBT(Btree, 4, 33); inorderBT(Btree.root);
+deleteBT(Btree, 4, 75); inorderBT(Btree.root);
+deleteBT(Btree, 4, 136); inorderBT(Btree.root);
+deleteBT(Btree, 4, 150); inorderBT(Btree.root);
+deleteBT(Btree, 4, 120); inorderBT(Btree.root);
+deleteBT(Btree, 4, 69); inorderBT(Btree.root);
+deleteBT(Btree, 4, 43); inorderBT(Btree.root);
+deleteBT(Btree, 4, 26); inorderBT(Btree.root);
+deleteBT(Btree, 4, 60); inorderBT(Btree.root);
+deleteBT(Btree, 4, 130); inorderBT(Btree.root);
+deleteBT(Btree, 4, 50); inorderBT(Btree.root);
+deleteBT(Btree, 4, 18); inorderBT(Btree.root);
+deleteBT(Btree, 4, 7); inorderBT(Btree.root);
+deleteBT(Btree, 4, 36); inorderBT(Btree.root);
+deleteBT(Btree, 4, 58); inorderBT(Btree.root);
 console.log();
